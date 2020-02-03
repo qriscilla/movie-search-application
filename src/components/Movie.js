95f673ba7c7f8ff488634be41f7d251434d38294
qriscilla <pri.whang@gmail.com>
2020-02-02 19:30:44 -0500

    Completed this version

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import noImage from './noImage.png';
 
 const API_KEY = '37cbb44eaa2f92cdb9ef7ba91a012e48';
 
@@ -22,15 +23,49 @@ class Movie extends React.Component {
         return (
             <div className='container'>
                 {this.state.activeMovie.length !== 0 &&
-                    <div>
-                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                        <h3>{movie.title}</h3>
-                        <h4>{movie.release_date}</h4>
-                        <p>{movie.overview}</p>
-                        <button>
-                            <Link to='/'>Go back</Link>
-                        </button>
-                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>
+                                    { movie.poster_path !== null 
+                                        ? (<img className='movie-img' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}></img>)
+                                        : (<img className='movie-img' src={noImage} alt={movie.title}></img>)
+                                    }
+                                </th>
+                                <th className='movie-card-info'>
+                                    <h3>{movie.title}</h3>
+                                    <h4>{movie.release_date.substring(0,4)}</h4>
+                                    <p>{movie.overview}</p>
+                                    <button className='movies-card-button'>
+                                        <Link to='/' style={{textDecoration:'none'}}>Go back</Link>
+                                    </button>
+                                </th>
+                            </tr>
+                        </thead>
+                    </table>
+
+
+                    // <div className='row movie-card'>
+                    //     <div className='col-md-6'>
+                    //         <img className='movie-img'
+                    //             src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+                    //             alt={movie.title} 
+                    //         />
+                    //     </div>
+                    //     <div className='col-md-6 movie-card-info'>
+                    //         <h3>{movie.title}</h3>
+                    //         <h4>{movie.release_date}</h4>
+                    //         <p>{movie.overview}</p>
+                    //         <button>
+                    //             <Link to='/'>Go back</Link>
+                    //         </button>
+                    //     </div>
+                    // </div>
+
+
+
+
                 }
             </div>
         );
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import noImage from './noImage.png';
 
 const Movies = props => (
     <div className='container'>
@@ -7,21 +8,24 @@ const Movies = props => (
             {props.movies.map((movie) => {
                 return (
                     <div key={movie.id} className='col-md-4' style={{marginBottom:'2rem'}}>
-                        <div className='movie-card'>
-                            <img 
-                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
-                                alt={movie.title}>
-                            </img>
-                            <div className='movie-card-info'>
-                                <h5 className='movie-card-title'>{movie.title.length < 30 ? `${movie.title}` : `${movie.title.substring(0,30)}...`}</h5>
-                                <p>({movie.release_date})</p>
+                        <div className='movies-card'>
+                            { movie.poster_path !== null 
+                                ? (<img className='movies-img' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}></img>)
+                                : (<img className='movies-img' src={noImage} alt={movie.title}></img>)
+                            }
+                            <div className='movies-card-info'>
+                                <h5 className='info'>{movie.title.length < 27 ? `${movie.title}` : `${movie.title.substring(0,27)}...`}</h5>
+                                <p className='info'>({movie.release_date.substring(0,4)})</p>
+                                <button className='movies-card-button'>
+                                    <Link 
+                                        className='movies-card-link' 
+                                        style={{textDecoration:'none'}}
+                                        to={{ 
+                                        pathname: `/movie/${movie.id}`,
+                                        state: {movie: movie.title}
+                                    }}>Details</Link>
+                                </button>
                             </div>
-                            <button className='movie-card-button'>
-                                <Link to={{ 
-                                    pathname: `/movie/${movie.id}`,
-                                    state: {movie: movie.title}
-                                }}>Details</Link>
-                            </button>
                         </div>
                     </div>
                 );
